3b1f55108d0fbb535c1fa81cf2bddfde68c791c5
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-11-06 09:19:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,8 +16,17 @@ function displayLiturgia(data) {
     document.getElementById('dia').textContent = `Dia: ${data.dia}`;
     document.getElementById('oferendas').textContent = `Oferendas: ${data.oferendas}`;
     document.getElementById('comunhao').textContent = `Comunhão: ${data.comunhao}`;
-    document.getElementById('primeiraLeitura').innerHTML = `<strong>Primeira Leitura:</strong> ${data.primeiraLeitura.referencia} - ${data.primeiraLeitura.titulo}<br>${data.primeiraLeitura.texto}`;
-    document.getElementById('segundaLeitura').innerHTML = `<strong>Segunda Leitura:</strong> ${data.segundaLeitura.referencia} - ${data.segundaLeitura.titulo}<br>${data.segundaLeitura.texto}`;
+
+    if (data.primeiraLeitura) {
+        document.getElementById('primeiraLeitura').innerHTML = `<strong>Primeira Leitura:</strong> ${data.primeiraLeitura.referencia} - ${data.primeiraLeitura.titulo}<br>${data.primeiraLeitura.texto}`;
+    }
+
+    if (data.segundaLeitura) {
+        document.getElementById('segundaLeitura').innerHTML = `<strong>Segunda Leitura:</strong> ${data.segundaLeitura.referencia} - ${data.segundaLeitura.titulo}<br>${data.segundaLeitura.texto}`;
+    } else {
+        document.getElementById('segundaLeitura').innerHTML = `<strong>Segunda Leitura:</strong> Não há segunda leitura hoje!`;
+    }
+
     document.getElementById('salmo').innerHTML = `<strong>Salmo:</strong> ${data.salmo.referencia} - ${data.salmo.refrao}<br>${data.salmo.texto}`;
     document.getElementById('evangelho').innerHTML = `<strong>Evangelho:</strong> ${data.evangelho.referencia} - ${data.evangelho.titulo}<br>${data.evangelho.texto}`;
 }
